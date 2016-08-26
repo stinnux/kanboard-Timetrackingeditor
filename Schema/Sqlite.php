9b367edce8661f62972d9e793bf11858d23cf32e
stinnux <me@stinnux.de>
2016-08-26 11:53:31 +0200

    Add schema definition for comment and is_billable

---
+++ Schema/Sqlite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kanboard\Plugin\Timetrackingeditor\Schema;
+
+const VERSION = 1;
+
+function version_1($pdo)
+{
+  $pdo->exec("ALTER TABLE subtask_time_tracking add comment TEXT");
+  $pdo->exec("ALTER TABLE subtask_time_tracking add is_billable INTEGER");
+}
