5cc69c6140585dd69b8b0037a7f86d34b27cd68a
Thomas Stinner <me@stinnux.de>
2016-10-26 15:09:59 +0200

    correct spelling

--- Model/SubtaskTimeTrackingModel.php
+++ Model/SubtaskTimeTrackingModel.php
@@ -10,7 +10,7 @@ use Kanboard\Model\UserModel;
  * @author Thomas Stinner
  */
 
-class SubtaskTimeTrackingModel extends \Kanboard\Model\SubtaskTimeTrackingModel
+class SubtasktimeTrackingModel extends \Kanboard\Model\SubtaskTimeTrackingModel
 {
   /**
    * Log start time
